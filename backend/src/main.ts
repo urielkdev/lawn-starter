8e90cd8a023521337a42a98ffc16474c9f1a810b
unknown <urielkindermann@gmail.com>
2025-06-02 21:28:49 -0300

    feat/backend: allow cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,10 @@
+import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppModule } from './app.module';
 
 const bootstrap = async () => {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(
     new ValidationPipe({
